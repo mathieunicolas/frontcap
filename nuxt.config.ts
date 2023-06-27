cb724098e98a6d7cde7b2e028996bf542b54598c
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-06-27 18:33:22 +0200

    deps & baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const base = process.env.NODE_ENV === 'development' ? '/' : '/p/nuxt'
+
 export default defineNuxtConfig({
   modules: ['@nuxthq/ui'],
   devtools: { enabled: true },
   ui: {
     safelistColors: ['primary', 'red', 'green', 'amber', 'yellow', 'blue', 'orange']
+  },
+  app: {
+    baseURL: base
   }
 })
